e869be45f3129bf5b1f2f790fbf51880d95feb91
Zohra Hussaini <zhussaini306@gmail.com>
2026-03-03 20:50:34 -0600

    Update first_script.py

--- Scripting Python Foundations/first_script.py
+++ Scripting Python Foundations/first_script.py
@@ -1,1 +1,26 @@
 
+favorite_movies = [
+  ("10 Things I Hate About You", 1999),
+  ("Harry Potter and the Sorcerer's Stone", 2001),
+  ("The Princess and the Frog", 2009),
+  ("The Dark Knight", 2008)
+]
+
+def check_movie(movie):
+  name = movie[0]
+  year = movie[1]
+
+  if year < 2000:
+    print(name + ": This movie was released before 2000")
+    return None
+  else:
+    print(name + ": This movie was released after 2000")
+    return name
+
+recent_movies = []
+
+for movie in favorite_movies:
+  result = check_movie(movie)
+
+  if result is not None:
+    recent_
