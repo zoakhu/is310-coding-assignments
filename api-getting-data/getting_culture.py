c77aad85bf14d6757725adce2b9826d137b7865b
Zohra Hussaini <zhussaini306@gmail.com>
2026-04-09 13:27:03 -0500

    Update getting_culture.py

--- api-getting-data/getting_culture.py
+++ api-getting-data/getting_culture.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-
+API_url = https://hp-api.onrender.com/
 hp_url = "https://hp-api.onrender.com/api/character/9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8"
 
 hp_response = requests.get(hp_url)
